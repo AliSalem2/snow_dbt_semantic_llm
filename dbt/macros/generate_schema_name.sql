11d9a0155ab6f44ecc8c92011f0f3ed11477af48
Ali Salem <leesalem@gmail.com>
2026-09-17 12:31:45 +0200

    Serve target, prod schemas and metric descriptions

--- dbt/macros/generate_schema_name.sql
+++ dbt/macros/generate_schema_name.sql
@@ -6,7 +6,7 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
     {%- if custom_schema_name is none -%}
         {{ target.schema }}
-    {%- elif target.name == 'prod' -%}
+    {%- elif target.name in ('prod', 'serve') -%}
         {{ custom_schema_name | trim }}
     {%- else -%}
         {{ target.schema }}_{{ custom_schema_name | trim }}
